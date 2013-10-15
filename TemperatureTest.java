a26d83df35f176acaf37fdc6a09337ed9bcf6491
Daniel Kent <daniel.dominic@mail.mcgill.ca>
2013-10-15 17:18:51 -0400

    Update TemperatureTest.java

--- TemperatureTest.java
+++ TemperatureTest.java
@@ -31,5 +31,34 @@ public class TemperatureTest {
 	public void getValue (){
 		Temperature test = new Temperature(0, Temperature.Units.KELVIN); //Tests to see if we get correct values
 		double result = test.getValue();
-		assertEquals(0, 0);}
+		assertEquals(0, 0);
+		}
+	
+	@Test
+	public void changeUnits (){
+		Temperature test = new Temperature(0, Temperature.Units.CELSIUS);
+		Temperature.Units result = test.changeUnits(Temperature.Units.FAHRENHEIT);
+		assertEquals(result, Temperature.Units.FAHRENHEIT);
+		
+		Temperature test1 = new Temperature(0, Temperature.Units.CELSIUS);
+		Temperature.Units result1 = test1.changeUnits(Temperature.Units.KELVIN);
+		assertEquals(result1, Temperature.Units.KELVIN);
+		
+		Temperature test2 = new Temperature(0, Temperature.Units.KELVIN);
+		Temperature.Units result2 = test2.changeUnits(Temperature.Units.FAHRENHEIT);
+		assertEquals(result2, Temperature.Units.FAHRENHEIT);
+		
+		Temperature test3 = new Temperature(0, Temperature.Units.KELVIN);
+		Temperature.Units result3 = test3.changeUnits(Temperature.Units.CELSIUS);
+		assertEquals(result3, Temperature.Units.CELSIUS);
+		
+		Temperature test4 = new Temperature(0, Temperature.Units.FAHRENHEIT);
+		Temperature.Units result4 = test4.changeUnits(Temperature.Units.CELSIUS);
+		assertEquals(result4, Temperature.Units.CELSIUS);
+		
+		Temperature test5 = new Temperature(0, Temperature.Units.FAHRENHEIT);
+		Temperature.Units result5 = test5.changeUnits(Temperature.Units.KELVIN);
+		assertEquals(result5, Temperature.Units.KELVIN);
+		}
+	
 	}
